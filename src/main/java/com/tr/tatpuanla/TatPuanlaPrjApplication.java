7afce52ae6e9c60875e306ef5cbc4efd69b9518e
talhakkoc <talhaakkoc@gmail.com>
2022-03-20 01:15:16 +0300

    first commit

---
+++ src/main/java/com/tr/tatpuanla/TatPuanlaPrjApplication.java
@@ -0,0 +1,13 @@
+package com.tr.tatpuanla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatPuanlaPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TatPuanlaPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tr/tatpuanla/TatPuanlaPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.tatpuanla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatPuanlaPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
